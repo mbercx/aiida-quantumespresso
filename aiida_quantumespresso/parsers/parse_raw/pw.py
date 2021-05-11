7c13af16807dd29ce1d7fc87cc832dd8651b9112
Marnik Bercx <mbercx@gmail.com>
2021-05-11 11:59:16 +0200

    update nlcg parsing

--- aiida_quantumespresso/parsers/parse_raw/pw.py
+++ aiida_quantumespresso/parsers/parse_raw/pw.py
@@ -924,26 +924,32 @@ def parse_nlcg(nlcg_out):
 
     logs = get_logging_container()
 
-    iteration_pattern = r'(\d+)\s+([\d.-]+e[+-]\d\d)\t([\d.-]+e[+-]\d\d)'
-    iteration_fields = ('Iteration', 'Free energy', 'Residual')
-    free_energy_pattern = r'F\s+:\s+([\d.-]+e[+-]\d\d)'
+    iteration_pattern = r'(\d+)\s+([\d.-]+)[\s\t]+([\d.-]+e[+-]\d\d)\s+([\d.-]+e[+-]\d\d)'
+    iteration_fields = ('iteration', 'free energy', 'wfc_slope', 'eta_slope')
+    free_energy_pattern = r'F\s*:\s+([\d.-]+)'
+    ks_energy_pattern = r'KS-energy\s*:\s+([\d.-]+)'
 
     iterations = []
     free_energy = None
+    ks_energy = None
     nlcg_converged = False
 
     for line in nlcg_out.splitlines():
         
         match_iteration = re.match(iteration_pattern, line)
         match_free_energy = re.match(free_energy_pattern, line)
+        match_ks_energy = re.match(ks_energy_pattern, line)
         
         if match_iteration:
             it = match_iteration.groups()
-            iteration_data = (int(it[0]), float(it[1]), float(it[2]))
+            iteration_data = (int(it[0]), float(it[1]), float(it[2]), float(it[3]))
             iterations.append(dict(zip(iteration_fields, iteration_data)))
             
         elif match_free_energy:
             free_energy = float(match_free_energy.groups()[0])
+
+        elif match_ks_energy:
+            ks_energy = float(match_ks_energy.groups()[0])
 
         if 'NLCG SUCCESS' in line:
             nlcg_converged = True
@@ -954,6 +960,7 @@ def parse_nlcg(nlcg_out):
     parsed_data = {
         'iterations': iterations,
         'free_energy': free_energy,
+        'ks_energy': ks_energy,
         'nlcg_converged': nlcg_converged
     }
 
